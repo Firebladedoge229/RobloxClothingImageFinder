7be040b795effba8c433a61c0ff1c02d9c7b03ee
Fireblade <72758695+Firebladedoge229@users.noreply.github.com>
2024-03-10 20:08:14 -0400

    Add support for retries

--- clothing.py
+++ clothing.py
@@ -1,14 +1,16 @@
 import re
-import time
-import requests
-import argparse
 import sys
+import time
+import argparse
+import requests
+import http.client
 
 def get_args():
     parser = argparse.ArgumentParser(description="Roblox Clothing Image Finder")
     parser.add_argument("-i", "--id", type=int, help="Sets the Clothing Id")
     parser.add_argument("-s", "--silent", action="store_true", help="Enables silent mode")
     parser.add_argument("-w", "--wait", type=float, default=1, help="Sets the time to wait between requests")
+    parser.add_argument("-r", "--retries", type=int, default=10, help="Sets the max amount of retries")
     return parser.parse_args()
 
 args = get_args()
@@ -20,30 +22,20 @@ if not any(vars(args).values()):
 shirt = args.id
 silent_mode = args.silent
 wait_time = args.wait
+max_retries = args.retries
 
 oldId = shirt
 imageId = None
 
-html = None
-
 try:
-    html = int(re.search(r'data-group-last-edited-by-id="(\d+)"', requests.get(f"https://www.roblox.com/catalog/{str(shirt)}").text).group(1))
-except:
-    pass
+    creator = int(requests.get(f"https://economy.roblox.com/v2/assets/{shirt}/details").json()["Creator"]["Id"])
+except KeyError:
+    print("Please provide arguments. Use -h or --help for more information.")
+    sys.exit()
 
-creator = None
-isPlayer = html is None
+retry_count = 0
 
-if isPlayer:
-    try:
-        creator = int(requests.get(f"https://economy.roblox.com/v2/assets/{shirt}/details").json()["Creator"]["Id"])
-    except KeyError:
-        print("Please provide arguments. Use -h or --help for more information.")
-        sys.exit()
-else:
-    creator = html
-
-while imageId is None:
+while imageId is None and retry_count < max_retries:
     try:
         if not silent_mode:
             print(f"Searching {str(oldId - 1)}..")
@@ -59,10 +51,18 @@ while imageId is None:
                 else:
                     print(f"Found Image: {str(oldId - 1)}")
                 imageId = oldId - 1
+        elif 400 <= request.status_code < 600:
+            print(f"Retrying.. [{retry_count+1}/{max_retries}]")
+            retry_count += 1
+            time.sleep(wait_time)
         else:
-            print(request.status_code)
+            print(f"{http.client.responses[request.status_code]}: {request.status_code}")
             break
     except Exception as exception:
         print(exception)
-    oldId -= 1
+    if isinstance(request, dict):
+        oldId -= 1
     time.sleep(wait_time)
+
+if retry_count >= max_retries:
+    print("Max retries reached. Exiting..")
