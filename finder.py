a14aa2979321ce44395a4f0ea7a6599a519302b5
Fireblade <72758695+Firebladedoge229@users.noreply.github.com>
2024-03-11 21:39:12 -0400

    New method (cont.)

--- finder.py
+++ finder.py
@@ -1,40 +1,23 @@
 import re
-import time
 import requests
 import http.client
 
-shirt = int(input("Enter Clothing Id: "))
+shirt = int(input("\033[96mEnter Clothing Id: \033[0m"))
 
-oldId = shirt
-imageId = None
-
-creator = int(requests.get(f"https://economy.roblox.com/v2/assets/{shirt}/details").json()["Creator"]["Id"])
-
-retry_count = 0
-
-while imageId == None and retry_count < 10:
+id = requests.get(f"https://assetdelivery.roblox.com/v1/assetId/{shirt}")
+if id.status_code == 200:
     try:
-        print(f"Searching {str(oldId - 1)}..")
-        request = requests.get(f"https://economy.roblox.com/v2/assets/{oldId - 1}/details")
-        if request.status_code == 200:
-            request = request.json()
-            clothingCreator = int(request["Creator"]["Id"])
-            print(f"{creator} v {clothingCreator}")
-            if clothingCreator == creator:
-                print(f"Found Image: {str(oldId - 1)}")
-                imageId = oldId - 1
-        elif 400 <= request.status_code < 600:
-            print(f"Retrying.. [{retry_count+1}/10]")
-            retry_count += 1
-            time.sleep(1)
-        else:
-            print(f"{http.client.responses[request.status_code]}: {request.status_code}")
-            break
+        id = id.json()["location"]
+        shirtRequest = requests.get(id)
+        shirt = re.search(r"http://www\.roblox\.com/asset/\?id=(\d+)", shirtRequest.text).group(1)
+        print(shirt)
     except Exception as exception:
-        print(exception)
-    if isinstance(request, dict):
-        oldId -= 1
-    time.sleep(1)
-
-if retry_count >= 10:
-    print("Max retries reached. Exiting...")
+        exception = str(exception).replace("'", '"')
+        if "location" in exception:
+            print(f"\033[31mError: \033[0m{exception}.\033[31m Are you sure the Clothing Id is valid?\033[0m")
+        elif shirtRequest and shirtRequest.status_code != 200:
+            print(f"\033[31mError while sending clothing request: \033[0m{exception}")
+        else:
+            print(f"\033[31mError: \033[0m{exception}")
+else:
+    print(f"\033[31m{http.client.responses[id.status_code]}: {id.status_code}")
